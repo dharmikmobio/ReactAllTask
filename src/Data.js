f26286e098105912ed9276877209fe13bd9c9af7
Dharmik <dharmikp.mobio@gmail.com>
2022-03-21 16:13:27 +0530

    Searchgallery

---
+++ src/Data.js
@@ -0,0 +1,88 @@
+import img1 from "./img/a.jpg";
+import img2 from "./img/a1.jpg";
+import img3 from "./img/b.jpg";
+import img4 from "./img/b1.jpg";
+// import img5 from "./img/bo.jpg";
+import img6 from "./img/c.jpg";
+import img7 from "./img/c1.jpg";
+import img8 from "./img/c2.jpg";
+import img9 from "./img/c3.jpg";
+import img10 from "./img/c4.jpg";
+import img11 from "./img/co2.jpg";
+
+
+
+
+
+const data = {
+    cardData:[
+        {
+            id:1,
+            img:img1,
+            title:'aeroplane',
+            desc:'desc 1',
+        },
+        {
+            id:2,
+            img:img2,
+            title:'aeroplane',
+            desc:'desc 2',
+        },
+        {
+            id:3,
+            img:img3,
+            title:'bike',
+            desc:'desc 3',
+        },
+        {
+            id:4,
+            img:img4,
+            title:'bike',
+            desc:'desc 4',
+        },
+        // {
+        //     id:5,
+        //     img:img5,
+        //     title:'boat',
+        //     desc:'desc 5',
+        // },
+        {
+            id:6,
+            img:img6,
+            title:'car',
+            desc:'desc 6',
+        },
+        {
+            id:7,
+            img:img7,
+            title:'car',
+            desc:'desc 7',
+        },
+        {
+            id:8,
+            img:img8,
+            title:'car',
+            desc:'desc 8',
+        },
+        {
+            id:9,
+            img:img9,
+            title:'car',
+            desc:'desc 9',
+        },
+        {
+            id:10,
+            img:img10,
+            title:'car',
+            desc:'desc 10',
+        },
+        {
+            id:11,
+            img:img11,
+            title:'boat',
+            desc:'desc 11',
+        },
+    ]
+}
+
+export default data;
---
+++ src/Search.js
@@ -0,0 +1,58 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React,{useState} from 'react'
+import Data from './Data.js'
+
+const Search = () => {
+    const [filter, setFilter] = useState("");
+    const SearchText = (e) =>{
+        setFilter(e.target.value)
+    }
+    let dataSearch = Data.cardData.filter( item => {
+        return Object.keys(item).some( key => 
+            item[key].toString().toLowerCase().includes(filter.toString().toLocaleLowerCase())
+             )
+    });
+    
+  return (
+    <>
+            <section className="py-4 container">
+            <div className="row justify-content-center">
+
+
+
+                    <div
+                    className ="col-12 mb-5">
+                    <div className ="mb-3 col-4 mx-auto text-center">
+                    <label className ="form-lable h4">Search </label>
+                    <input
+                    type="text"
+                    className="from-control"
+                    value={filter}
+                    onChange={SearchText.bind(this)}
+                    />
+                    </div>
+                    </div>
+            {
+                dataSearch.map((item,index)=>{
+                   return(
+                            <div className="col-11 col-md-6 col-lg-3 mx-0 mb-4" >
+                            <div className="card p-0 overflow-hidden h-100 shadow">
+                            <img src={item.img} className="card-img-top" />
+                            {/* <div className="card-body">
+                    <h5 className="card-title">{item.title} </h5>
+                    <p className= "card-text">{item.desc} </p>
+                            </div> */}
+                            </div>
+                            </div>
+                         
+                   )
+               }) 
+            }
+           
+            </div>
+            </section>
+    </>
+  )
+}
+
+export default Search
